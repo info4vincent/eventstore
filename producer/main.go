5f400b161879b10b655de11117e1bec03d6ecfc0
info4vincent <info4vincent@gmail.com>
2017-11-05 14:31:09 +0100

    added producer for simulating an eventsource

---
+++ producer/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	zmq "github.com/pebbe/zmq4"
+
+	"fmt"
+)
+
+func main() {
+	//  Socket to talk to server
+	fmt.Println("Connecting to eventsource server...")
+	requester, _ := zmq.NewSocket(zmq.REQ)
+	defer requester.Close()
+	requester.Connect("tcp://192.168.2.11:5555")
+
+	// send hello
+	msg := fmt.Sprintf("CardScanned:%s", "000001")
+	fmt.Println("Sending ", msg)
+	requester.Send(msg, 0)
+
+	// Wait for reply:
+	reply, _ := requester.Recv(0)
+	fmt.Println("Received ", reply)
+}
