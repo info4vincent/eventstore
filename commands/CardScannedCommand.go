9c4943f0fc81b7e67945045a2a64f76e4e85548f
info4vincent <info4vincent@gmail.com>
2017-11-12 07:45:31 +0000

    improved reading of unknown cards and read it out loud

--- commands/CardScannedCommand.go
+++ commands/CardScannedCommand.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"bytes"
 	"crypto/tls"
 	"fmt"
 	"log"
@@ -62,6 +63,19 @@ func connectDB() *mgo.Session {
 	return session
 }
 
+func insertNth(s string, n int) string {
+	var buffer bytes.Buffer
+	var n_1 = n - 1
+	var l_1 = len(s) - 1
+	for i, rune := range s {
+		buffer.WriteRune(rune)
+		if i%n == n_1 && i != l_1 {
+			buffer.WriteRune('-')
+		}
+	}
+	return buffer.String()
+}
+
 func GetMessageOfUserForEvent(session *mgo.Session, uid string) CardUID {
 	c := session.DB(DBName).C(CollectionName)
 
@@ -70,16 +84,19 @@ func GetMessageOfUserForEvent(session *mgo.Session, uid string) CardUID {
 	err = c.Find(bson.M{"uid": uid}).All(&results)
 
 	log.Println("aantal results:", len(results))
+	readAbleUid := insertNth(uid, 1)
+	actionUri := fmt.Sprintf("Say:Hoi, deze kaart ken ik niet. Het nummer van de kaart is:%v", readAbleUid)
 	if err == nil {
 		if len(results) > 0 {
+			log.Println("Card found returning details now.")
 			return results[0]
 		} else {
 			log.Println("Could not find card")
-			return CardUID{uid, uid, uid, uid}
+			return CardUID{uid, uid, uid, actionUri}
 		}
 	} else if err != nil {
 		log.Println("Could not find card")
-		return CardUID{uid, uid, uid, uid}
+		return CardUID{uid, uid, uid, actionUri}
 	}
 
 	return results[0]
